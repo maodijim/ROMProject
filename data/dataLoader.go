97701abb60d390e284ee3e6e531998f60a7ba03d
Andy Mao <dmao1@mail.sfsu.edu>
2021-06-29 22:30:44 -0700

    upgrade go version to 1.16 and introduce go embed

---
+++ data/dataLoader.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	_ "embed"
+)
+
+//go:embed buff.json
+var BuffJson []byte
+
+//go:embed exchangeItems.json
+var ExchangeJson []byte
+
+//go:embed items.json
+var ItemsJson []byte
+
+//go:embed skills.json
+var SkillsJson []byte
