cd3a25d5341e1d7b8f9d841c0940736d112433d8
maodijim <17071934+maodijim@users.noreply.github.com>
2025-11-20 20:40:14 -0800

    feat: add account registration script with random username and password generation

---
+++ tools/private-server/register_account_jayc.py
@@ -0,0 +1,42 @@
+import time
+import random
+import string
+
+import requests
+
+register_url = "http://103.70.76.250/register/api.php"
+
+if __name__ == '__main__':
+    invite_code = input("请输入邀请码：")
+    for i in range(95):
+        letters = string.ascii_lowercase
+        number = string.digits
+        username = ''.join(random.choice(letters) for i in range(8))
+        password = ''.join(random.choice(number) for i in range(8))
+        data = {
+            "account": username,
+            "password": password,
+            "invitecode": invite_code,
+        }
+        print(data)
+        response = requests.post(
+            register_url,
+            data=data,
+            params={"do": "1"},
+            headers={
+                "Content-Type": "application/json; charset=UTF-8",
+                "Accept": "application/json, text/javascript, */*; q=0.01",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+            }
+        )
+        print(response.text)
+        if response.status_code == 200:
+            print("注册成功")
+            csv_file = open("account.csv", "a")
+            csv_file.write('\n')
+            csv_file.write(username + "," + password)
+        else:
+            print("注册失败")
+        time.sleep(1)
