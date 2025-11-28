c47d1acb89fa93f55c53cba439ace4c82982972f
maodijim <17071934+maodijim@users.noreply.github.com>
2025-11-27 23:00:50 -0800

    feat: enhance account registration with improved response handling and JSON support

--- tools/private-server/register_account_jayc.py
+++ tools/private-server/register_account_jayc.py
@@ -1,6 +1,7 @@
 import time
 import random
 import string
+import json
 
 import requests
 
@@ -31,7 +32,10 @@ if __name__ == '__main__':
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
             }
         )
-        res_body = response.json()
+        print(response.status_code)
+        body = response.text
+        print(body)
+        res_body = json.loads(body)
         print(res_body)
         if response.status_code == 200 and res_body.get("code") != 2:
             print("注册成功")
@@ -39,6 +43,25 @@ if __name__ == '__main__':
             csv_file.write('\n')
             csv_file.write(username + "," + password)
             csv_file.close()
+            data = {
+                "account": username,
+                "password": password,
+            }
+            response = requests.post(
+                register_url,
+                json=data,
+                params={"do": "2"},
+                headers={
+                    "Content-Type": "application/json; charset=UTF-8",
+                    "Accept": "application/json, text/javascript, */*; q=0.01",
+                    "Accept-Encoding": "gzip, deflate",
+                    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+                }
+            )
+            print(response.status_code)
+            body = response.text
+            print(body)
         else:
             print("注册失败")
         time.sleep(1)
