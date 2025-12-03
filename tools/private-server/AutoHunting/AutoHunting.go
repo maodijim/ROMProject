5b09ad6c72955ffcee4603aacbc252f008c9c5e4
Guei <sdfoa198911@yahoo.com.tw>
2025-12-04 02:02:37 +0800

    新增研究所3F自动行走

--- tools/private-server/AutoHunting/AutoHunting.go
+++ tools/private-server/AutoHunting/AutoHunting.go
@@ -43,7 +43,7 @@ func i32(v int32) *int32 { return &v }
 
 var Pos_03 = []Cmd.ScenePos{
 	{X: i32(21948), Y: i32(-583), Z: i32(43399)},
-	{X: i32(-14088), Y: i32(73), Z: i32(-56505)},
+	{X: i32(-14088), Y: i32(357), Z: i32(-56505)},
 }
 
 func (b *HuntTask) Start() {
@@ -86,7 +86,7 @@ func (b *HuntTask) StartHunt() {
 		b.EnableGodMode()
 	}
 
-	ticker := time.NewTicker(time.Second * 5)
+	ticker := time.NewTicker(time.Second * 1)
 	ticker2 := time.NewTicker(time.Second * 30)
 
 	targetId := uint64(0)
@@ -97,11 +97,15 @@ func (b *HuntTask) StartHunt() {
 				b.logger.Info("停止自动狩猎任务监控协程1")
 				return
 			case <-ticker.C:
-				if targetId != 0 && b.GC.AtkStat.GetCurrentTargetId() == targetId && time.Since(lastPosUpdate) > time.Second*3 {
+				if targetId != 0 && b.GC.AtkStat.GetCurrentTargetId() == targetId && time.Since(lastPosUpdate) > time.Second*10 {
 					log.Infof("卡住了")
 					b.GC.AtkStat.SetCurrentTargetId(0)
 					targetId = 0
 					b.useFlyWing()
+				} else if b.fightStar && targetId == 0 && time.Since(lastPosUpdate) > time.Second*10 {
+					b.logger.Infof("没有目标卡住了")
+					b.useFlyWing()
+					lastPosUpdate = time.Now()
 				} else if b.GC.AtkStat.GetCurrentTargetId() != targetId {
 					targetId = b.GC.AtkStat.GetCurrentTargetId()
 					lastPosUpdate = time.Now()
@@ -126,20 +130,27 @@ func (b *HuntTask) StartHunt() {
 		if b.GC.Role.GetMapId() != MapID {
 			if MapID == gameTypes.MapId_LhzDun03.Uint32() {
 				b.GC.GoToMap(gameTypes.MapId_LhzDun01.Uint32())
-				b.GC.ChangeMap(gameTypes.MapId_LhzDun01.Uint32())
-				for i := 0; i < len(b.GC.Configs.HuntConfig.TargetItems); i++ {
-					b.GC.MoveChartWait(Pos_03[i])
-				}
+				time.Sleep(time.Millisecond * 500)
+				b.GC.MoveChartWait(Pos_03[0])
+				time.Sleep(time.Millisecond * 500)
+				b.GC.ExitMapPos(gameTypes.MapId_LhzDun01.Uint32(), 2, b.GC.Role.GetPos())
+				time.Sleep(time.Millisecond * 500)
+				b.GC.MoveChartWait(Pos_03[1])
+				time.Sleep(time.Millisecond * 500)
+				b.GC.ExitMapPos(gameTypes.MapId_LhzDun02.Uint32(), 3, b.GC.Role.GetPos())
+				time.Sleep(time.Millisecond * 500)
 			} else {
 				b.GC.GoToMap(MapID)
-				b.GC.ChangeMap(MapID)
 			}
 
 			b.EnableGodMode()
 
 		} else {
-			if b.GC.Configs.HuntConfig.TimerFly > 0 && time.Since(lastFlyTime) > time.Second*time.Duration(b.GC.Configs.HuntConfig.TimerFly) {
+			if b.GC.Configs.HuntConfig.UseDoubleEXP && b.GC.Role.GetBuffById(6062) == nil {
+				b.UsesEXP()
+			} else if b.GC.Configs.HuntConfig.TimerFly > 0 && time.Since(lastFlyTime) > time.Second*time.Duration(b.GC.Configs.HuntConfig.TimerFly) {
 				b.useFlyWing()
+				lastFlyTime = time.Now()
 			} else if !b.GC.IsMonsterInRange(b.GC.Configs.HuntConfig.TargetMonsters...) {
 				log.Infof("没有找到目标怪物")
 				b.useFlyWing()
@@ -147,7 +158,9 @@ func (b *HuntTask) StartHunt() {
 				log.Infof("附近找到目标怪物，开始自动挂机，坐稳了")
 				b.fightCtx, b.fightCancel = context.WithCancel(context.Background())
 				b.GC.EnableAutoAttack(b.fightCtx, b.GC.Configs.HuntConfig.TargetMonsters...)
+				b.GC.Role.SetSkillCd(50057001, time.Now().Add(time.Second*4))
 				b.fightStar = true
+				lastPosUpdate = time.Now()
 			}
 		}
 
@@ -179,6 +192,10 @@ func (b *HuntTask) useFlyWing() {
 		log.Warn("没有找到苍蝇翅膀")
 		_ = b.GC.GetMainPackItems()
 	}
+	b.fightCancel()
+	b.fightStar = false
+	lastFlyTime = time.Now()
+	time.Sleep(time.Second * 2)
 }
 
 func (b *HuntTask) buyFlyWing() {
@@ -208,6 +225,18 @@ func (b *HuntTask) Useskill() {
 		Dir:    &dir,
 	}
 	b.GC.SkillCmd(10020001, pData, true)
+}
+
+func (b *HuntTask) UsesEXP() {
+	log.Infof("使用羊羊助力!")
+	num := int32(1)
+	dir := int32(utils.GetNpcDataValByType(b.GC.Role.UserDatas, Cmd.EUserDataType_EUSERDATATYPE_DIR))
+	pData := &Cmd.PhaseData{
+		Number: &num,
+		Pos:    b.GC.Role.Pos,
+		Dir:    &dir,
+	}
+	b.GC.SkillCmd(1556001, pData, true)
 }
 
 func NewHuntTask(ctx context.Context, gc *gameConnection.GameConnection) *HuntTask {
