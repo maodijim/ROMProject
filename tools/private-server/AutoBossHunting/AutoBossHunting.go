458690be535bddd72c324a8ef552d7ae0036448e
Guei <sdfoa198911@yahoo.com.tw>
2025-12-09 01:12:04 +0800

    Fix the issue with checking the target BOSS's dead state

--- tools/private-server/AutoBossHunting/AutoBossHunting.go
+++ tools/private-server/AutoBossHunting/AutoBossHunting.go
@@ -389,7 +389,7 @@ func (b *BossHuntTask) checkTargetBossLive() bool {
 		if v.GetId() == b.targetMonster.GetId() && v.GetMapid() == b.targetMonster.GetMapid() {
 			if v.RefreshTime == nil {
 				return true
-			} else if v.Settime != nil {
+			} else if v.Settime != nil || *v.Settime != 0 {
 				return false
 			} else {
 				past, _ := IsPastTime(v.GetRefreshTime())
